f821b1d930d058d205e2880ad1905ce7127ab156
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-10 23:06:29 -0400

    first release

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 'use strict';
 
-module.exports = function() {
+var extend = require('extend-shallow');
+var Normalize = require('normalize-pkg');
+var through = require('through2');
+
+module.exports = function(options) {
+  var opts = extend({}, options);
+
+  var isMatch = opts.match || function isMatch(file) {
+    return file.basename === 'package.json';
+  };
+
+  return through.obj(function(file, enc, next) {
+    if (!isMatch(file)) {
+      next(null, file);
+      return;
+    }
+
+    if (file.isNull()) {
+      next(null, file);
+      return;
+    }
+
+    var normalizer = new Normalize(opts);
+    var str = file.contents.toString();
+    var pkg = normalizer.normalize(JSON.parse(str));
+    file.contents = new Buffer(JSON.stringify(pkg, null, 2));
+    next(null, file);
+  });
 };
--- test.js
+++ test.js
@@ -1,28 +1,150 @@
 'use strict';
 
 require('mocha');
+var fs = require('fs');
+var path = require('path');
 var assert = require('assert');
+var File = require('vinyl');
 var normalize = require('./');
+
+function readExpected(filepath) {
+  var cwd = path.resolve(__dirname, 'expected', path.basename(filepath));
+  return fs.readFileSync(cwd, 'utf8');
+}
 
 describe('gulp-normalize-pkg', function() {
   it('should export a function', function() {
     assert.equal(typeof normalize, 'function');
   });
 
-  it('should export an object', function() {
-    assert(normalize);
-    assert.equal(typeof normalize, 'object');
+  it('should return a stream', function() {
+    var stream = normalize();
+    assert(stream);
+    assert.equal(typeof stream, 'object');
+    assert.equal(typeof stream.pipe, 'function');
   });
 
-  it('should throw an error when invalid args are passed', function(cb) {
-    try {
-      normalize();
-      cb(new Error('expected an error'));
-    } catch (err) {
-      assert(err);
-      assert.equal(err.message, 'expected first argument to be a string');
-      assert.equal(err.message, 'expected callback to be a function');
+  it('should not fail on non-existent files', function(cb) {
+    var stream = normalize();
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: path.resolve(__dirname, 'foofofofof/package.json')
+    }));
+
+    stream.on('data', function(file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function() {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'foofofofof/package.json');
       cb();
-    }
+    });
+
+    stream.end();
+  });
+
+  it('should normalize a package.json', function(cb) {
+    var stream = normalize();
+
+    var buffer = [];
+    var filepath = path.resolve(__dirname, 'fixtures/package.json');
+    var expected = readExpected(filepath);
+
+    stream.write(new File({
+      base: __dirname,
+      path: filepath,
+      contents: fs.readFileSync(filepath)
+    }));
+
+    stream.on('data', function(file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function() {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].contents.toString(), expected);
+      assert.equal(buffer[0].relative, 'fixtures/package.json');
+      cb();
+    });
+
+    stream.end();
+  });
+
+  it('should use custom normalizer fields', function(cb) {
+    var stream = normalize({
+      fields: {
+        foo: {
+          type: ['array', 'string'],
+          normalize: function() {
+            return 'bar';
+          }
+        }
+      }
+    });
+
+    var buffer = [];
+    var filepath = path.resolve(__dirname, 'fixtures/package.json');
+    var expected = readExpected('custom.json');
+
+    stream.write(new File({
+      base: __dirname,
+      path: filepath,
+      contents: fs.readFileSync(filepath)
+    }));
+
+    stream.on('data', function(file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function() {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].contents.toString(), expected);
+      assert.equal(buffer[0].relative, 'fixtures/package.json');
+      cb();
+    });
+
+    stream.end();
+  });
+
+  it('should overwrite default normalizer fields', function(cb) {
+    var stream = normalize({
+      fields: {
+        name: {
+          type: ['string'],
+          normalize: function(val, key, config) {
+            if (!/-foo$/.test(val)) {
+              val += '-foo';
+            }
+            return val;
+          }
+        }
+      }
+    });
+
+    var buffer = [];
+    var filepath = path.resolve(__dirname, 'fixtures/package.json');
+    var expected = readExpected('overwrite.json');
+
+    stream.write(new File({
+      base: __dirname,
+      path: filepath,
+      contents: fs.readFileSync(filepath)
+    }));
+
+    stream.on('data', function(file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function() {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].contents.toString(), expected);
+      assert.equal(buffer[0].relative, 'fixtures/package.json');
+      cb();
+    });
+
+    stream.end();
   });
 });
